4b8966efd912720a19256ee969e90a70b511c89b
stefanristic40 <stefanristic407@gmail.com>
2024-02-12 11:06:08 -0500

    Remove package.lock

--- src/containers/FooterContainer/FooterContainer.js
+++ src/containers/FooterContainer/FooterContainer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useConfiguration } from '../../context/configurationContext';
 import loadable from '@loadable/component';
 
+import './FooterContainer.css';
+
 const SectionBuilder = loadable(
   () => import(/* webpackChunkName: "SectionBuilder" */ '../PageBuilder/PageBuilder'),
   {
