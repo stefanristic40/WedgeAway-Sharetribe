5e76f52ab22d4dd6878759e3a8866056486b6c9e
stefanristic40 <stefanristic407@gmail.com>
2024-02-13 12:45:02 -0500

    Prime section css

--- src/containers/LandingPage/sections/Section7.js
+++ src/containers/LandingPage/sections/Section7.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import './Section7.css';
+import css from './Section7.module.css';
 
 function Section7() {
   return (
-    <div className="section7 n-container">
-      <h1>It’s Prime Golf Season for These Warm Destinations</h1>
-      <div className="items">
-        <div className="item">
-          <img src="/static/images/locations/florida.png" alt="florida" />
-          <h3>Florida</h3>
-        </div>
-        <div className="item">
-          <img src="/static/images/locations/lasvegas.png" alt="lasvegas" />
-          <h3>Las Vegas</h3>
-        </div>
-        <div className="item">
-          <img src="/static/images/locations/hawaii.png" alt="hawaii" />
-          <h3>Hawaii</h3>
+    <div className={css.section7}>
+      <div className="n-container">
+        <h1>It’s Prime Golf Season for These Warm Destinations</h1>
+        <div className={css.items}>
+          <div className={css.item}>
+            <img
+              className={css.season_img}
+              src="/static/images/locations/florida.png"
+              alt="florida"
+            />
+            <h3>Florida</h3>
+          </div>
+          <div className={css.item}>
+            <img
+              className={css.season_img}
+              src="/static/images/locations/lasvegas.png"
+              alt="lasvegas"
+            />
+            <h3>Las Vegas</h3>
+          </div>
+          <div className={css.item}>
+            <img
+              className={css.season_img}
+              src="/static/images/locations/hawaii.png"
+              alt="hawaii"
+            />
+            <h3>Hawaii</h3>
+          </div>
         </div>
       </div>
     </div>
