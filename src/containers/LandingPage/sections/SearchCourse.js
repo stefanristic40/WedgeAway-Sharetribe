c998151435870bfdda2e93a26e23040343e2c127
stefanristic40 <stefanristic407@gmail.com>
2024-02-16 14:50:52 -0500

    Fix date picker import and update search course section

--- src/containers/LandingPage/sections/SearchCourse.js
+++ src/containers/LandingPage/sections/SearchCourse.js
@@ -4,17 +4,19 @@ import { CiSearch } from 'react-icons/ci';
 
 function SearchCourse(props) {
   return (
-    <div className="n-container">
-      <div className={css.searchCourse}>
-        <div>
-          <h3>
-            Looking To Rent Clubs By Courses You’re Playing? Search Thousands of Courses to Find
-            Club Rentals Nearby
-          </h3>
-        </div>
-        <div className={css.search_box}>
-          <input className={css.search} type="text" placeholder="Search by Course" />
-          <CiSearch size={30} color="white" className={css.search_icon} />
+    <div className={css.searchCourseContainer}>
+      <div className="n-container">
+        <div className={css.searchCourse}>
+          <div>
+            <h3>
+              Looking To Rent Clubs By Courses You’re Playing? Search Thousands of Courses to Find
+              Club Rentals Nearby
+            </h3>
+          </div>
+          <div className={css.search_box}>
+            <input className={css.search} type="text" placeholder="Search by Course" />
+            <CiSearch size={30} color="white" className={css.search_icon} />
+          </div>
         </div>
       </div>
     </div>
